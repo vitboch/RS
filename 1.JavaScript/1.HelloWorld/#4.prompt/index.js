8d10dcbf8ea00d8214d7ce2dea82f5d4e1fc4344
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-07-04 15:41:04 +0700

    Math questions and answers

---
+++ 1.JavaScript/1.HelloWorld/#4.prompt/index.js
@@ -0,0 +1,8 @@
+prompt('How much is 2 plus 2?')
+alert('If you answered 4, then you are right')
+
+prompt('What is the square root of 9?')
+alert('If you answered 3, then you are right')
+
+prompt('How much is 2 to the 5th power?')
+alert('If you answered 32, then you are right')
