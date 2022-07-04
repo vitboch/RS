5179e72f11534810450293c6962e7602824a1a08
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-07-04 20:27:06 +0700

    change variable values

---
+++ 1.JavaScript/2.Syntax/#4.variables/car.js
@@ -0,0 +1,21 @@
+const carName = 'Toyota'
+let maxCarSpeed = 180
+let carOwner = 'Vladislav'
+
+maxCarSpeed = 175
+console.log('The car\'s speed has changed:', maxCarSpeed)
+
+maxCarSpeed = 170
+console.log('The car\'s speed has changed:', maxCarSpeed)
+
+maxCarSpeed = 165
+console.log('The car\'s speed has changed:', maxCarSpeed)
+
+carOwner = 'Alexandr'
+console.log('The car has changed ownership:', carOwner)
+
+carOwner = 'Ivan'
+console.log('The car has changed ownership:', carOwner)
+
+carOwner = 'Igor'
+console.log('The car has changed ownership:', carOwner)
