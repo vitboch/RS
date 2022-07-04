da7a3002a3db3b03b7704d7589e98e77cefdfcd3
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-07-04 15:23:45 +0700

    Page about JavaScript

---
+++ 1.JavaScript/1.HelloWorld/#2#3.firstProject/js-info.js
@@ -0,0 +1,14 @@
+alert('Who developed the JavaScript programming language?')
+alert('JavaScript was created by Brendan Eich at Netscape')
+
+alert('In how many it took to develop JavaScript?')
+alert('It took Eich only 10 days to develop the scripting language')
+
+alert('In what year was JavaScript introduced?')
+alert('JavaScript was created in September 1995')
+
+alert('Does JavaScript mean the same as Java?')
+alert('JavaScript is not Java')
+
+alert('What is the name of the JavaScript specification?')
+alert('ECMAScript')
