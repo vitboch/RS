11f0be89e368ef6a32c4ff8d1fe9ec4a05f7b219
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-07-04 20:43:20 +0700

    const

---
+++ 1.JavaScript/2.Syntax/#5.variables/const.js
@@ -0,0 +1,11 @@
+const question1 = 'What is the smallest country in the world by population?'
+const question2 = 'What is the largest country in the world by population?'
+const question3 = 'What is the largest continent on the planet?'
+
+const answer1 = 'China'
+const answer2 = 'Eurasia'
+const answer3 = 'Vatican'
+
+console.log(question1, 'Answer:', answer3)
+console.log(question2, 'Answer:', answer1)
+console.log(question3, 'Answer:', answer2)
