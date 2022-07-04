f966ccf5a87db17cdeb79a4328041827524e2e09
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-07-04 19:21:35 +0700

    const let

---
+++ 1.JavaScript/2.Syntax/#2.variables/index.js
@@ -0,0 +1,11 @@
+const myName = 'Vitaly'
+const mySurname = 'Bochkarev'
+let myFavoriteDrink = 'tea'
+let myFavoriteAnimal = 'cat'
+let myFavoriteProgrammingLanguage = 'JavaScript'
+
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
