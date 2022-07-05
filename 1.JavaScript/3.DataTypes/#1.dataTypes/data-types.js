c045844745b365398dce2df34f52d29c7e686031
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-07-05 07:33:10 +0700

    8 types

---
+++ 1.JavaScript/3.DataTypes/#1.dataTypes/data-types.js
@@ -0,0 +1,35 @@
+// number,
+const age = 51
+console.log(typeof age)
+
+// string
+const name = 'Elon Reeve Musk'
+console.log(typeof name)
+
+// boolean
+const engineer = true
+console.log(typeof engineer)
+
+// null
+const presidentUSA = null
+console.log(typeof presidentUSA)
+
+// undefined
+const showman = undefined
+console.log(typeof showman)
+
+// object
+const person = {
+    name: 'William Henry Gates III',
+    age: 66,
+    philanthropist: true
+}
+console.log(typeof person)
+
+// bigint,
+const bigint = 100n
+console.log(typeof bigint)
+
+// symbol
+const id = Symbol('id')
+console.log(typeof id)
