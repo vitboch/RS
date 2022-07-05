6eeb726f90b7ee5df673e846713f7d389362e9c2
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-07-05 08:05:51 +0700

    number to string

---
+++ 1.JavaScript/3.DataTypes/#2.changeDataType/index.js
@@ -0,0 +1,11 @@
+let year = 2022
+year = 'We are already in the middle of 2022'
+alert(year)
+
+let months = '6 months left until the end of the year'
+months = 6
+alert(months)
+
+let magicExists = true
+magicExists = null
+alert(magicExists)
