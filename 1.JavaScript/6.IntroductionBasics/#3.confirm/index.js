6df0eb562a2811bdf06af5067d364d5e582b7ac7
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-07-07 00:04:19 +0700

    ? :

---
+++ 1.JavaScript/6.IntroductionBasics/#3.confirm/index.js
@@ -0,0 +1,8 @@
+const question1 = confirm('JavaScript appeared in 1995?')
+question1 ? alert('Right') : alert('Wrong, JavaScript appeared in 1995')
+
+const question2 = confirm('Is the JavaScript specification called ECMAScript?')
+question2 ? alert('Right') : alert('Wrong, the JavaScript specification is called ECMAScript')
+
+const question3 = confirm('JavaScript appeared in 1995?')
+question3 ? alert('Wrong, it was created in 10 days') : alert('Right')
