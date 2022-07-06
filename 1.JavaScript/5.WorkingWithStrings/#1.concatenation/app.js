dbdb5a76720d3615951adf84bdc4c67807be4f79
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-07-06 07:24:50 +0700

    template literals

---
+++ 1.JavaScript/5.WorkingWithStrings/#1.concatenation/app.js
@@ -0,0 +1,14 @@
+const myName = 'Vitaly'
+const programmingLanguage = 'JavaScript'
+const courseCreatorName = 'Vladilen'
+const reasonText = 'it\'s exciting!'
+const numberOfMonth = 'seven'
+
+const text = `Hello everyone!
+My name is ${myName}.
+I am currently studying ${programmingLanguage} in the programmingLanguage course with ${courseCreatorName}.
+I want to become a web developer because ${reasonText}.
+Before that, I studied ${programmingLanguage} ${numberOfMonth} months.
+I am sure that I will pass this course to end!`
+
+console.log(text)
