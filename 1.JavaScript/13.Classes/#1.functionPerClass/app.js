674f40355234b78b875993211c6d9d83d4d0417d
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-07-26 13:36:16 +0700

    class this new

---
+++ 1.JavaScript/13.Classes/#1.functionPerClass/app.js
@@ -0,0 +1,21 @@
+class Student {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+        this.technologies = []
+        this.status = 'Junior'
+    }
+
+    setTechnologies = function (technologies) {
+        this.technologies = [...this['technologies'], ...technologies]
+    }
+
+    setNewStatus = function (newStatus) {
+        this.status = newStatus
+    }
+}
+
+const student = new Student('Maxim', 20)
+student.setTechnologies(['HTML', 'CSS', 'JavaScript'])
+student.setNewStatus('Middle')
+console.log(student)
